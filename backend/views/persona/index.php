1fbf4737957cf3f0b0feb6995a98bf809233e8bf
sistemas <rodri_25193@hotmail.com>
2020-02-21 10:52:45 -0300

    se quita id de la vista principal

--- backend/views/persona/index.php
+++ backend/views/persona/index.php
@@ -26,7 +26,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id_persona',
+            //'id_persona',
             'nombre_persona',
 
             ['class' => 'yii\grid\ActionColumn'],
